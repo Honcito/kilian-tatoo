39cea5a07256b7d77cd958a3bef5b27251067be4
Hong <vhoncito@gmail.com>
2025-02-18 15:37:20 +0000

    Conflicts rebase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+>>>>>>> 4867131 (working on Hero.js)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+<<<<<<< HEAD
 );
+=======
+);
+>>>>>>> 4867131 (working on Hero.js)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
@@ -7,3 +8,43 @@ module.exports = {
   plugins: [],
 };
 
+=======
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      primary: 'Oswald',
+      secondary: 'Raleway',
+      tertiary: 'Rozha One',
+    },
+    container: {
+      padding: {
+        DEFAULT: '20px',
+        lg: '0',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1300px',
+    },
+    extend: {
+      colors: {
+        dark: '#111111',
+        grey: {
+          DEFAULT: '#555555',
+        },
+      },
+      backgroundImage: {
+        hero: "url('/src/img/hero/bg.jpg')",
+        about: "url('/src/img/about/image.png')",
+        interview: "url('/src/img/interview/bg.png')",
+      },
+      content: {
+        outlineText: 'url("/src/img/hero/outline-text.svg")',
+      },
+    },
+  },
+  plugins: [],
+};
+>>>>>>> 4867131 (working on Hero.js)
